4838ecba1d4f42d99582e757e593d8ed2ebd5ca8
Jungeun Choi <jungeun5.choi@gmail.com>
2023-08-02 15:12:03 +0900

    [unrated] Title: 문자열 돌리기, Time: 139.35 ms, Memory: 65.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+        for (int i = 0; i < str.length(); i++) {
+            sb.append(str.substring(i, i + 1)).append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
