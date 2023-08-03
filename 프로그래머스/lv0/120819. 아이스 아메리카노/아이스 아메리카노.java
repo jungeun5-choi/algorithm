5231fc4bc0bd4bdf4b195f58afbaa96270786bc8
Jungeun Choi <jungeun5.choi@gmail.com>
2023-08-03 16:50:08 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,11 +1,5 @@
 class Solution {
-    public int[] solution(int money) {
-        int[] answer = {};
-        int ice = 5500;
-        if (money < 5500) answer = new int[]{0, money};
-        else {
-            answer = new int[]{money / ice, money % ice};
-        }
-        return answer;
+    public int[] solution(int money) {        
+        return new int[]{money / 5500, money % 5500};
     }
 }
