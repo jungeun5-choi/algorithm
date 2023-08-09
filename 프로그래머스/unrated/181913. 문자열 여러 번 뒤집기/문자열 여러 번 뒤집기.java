fd09a4dffd49d51897e48c6f9393aaaa35354eb6
Jungeun Choi <jungeun5.choi@gmail.com>
2023-08-09 22:20:11 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 2.84 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int[][] queries) {
+        StringBuilder sb = new StringBuilder(my_string);
+        for (int[] query : queries) {
+            String substring = sb.substring(query[0], query[1] + 1);
+            String reversed = new StringBuilder(substring).reverse().toString();
+            sb.replace(query[0], query[1] + 1, reversed);
+        }
+        return String.valueOf(sb);
+    }
+}
