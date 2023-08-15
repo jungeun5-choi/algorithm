af303aa1cdb7128ea2007ad199496ed9562d3bf7
Jungeun Choi <jungeun5.choi@gmail.com>
2023-08-15 16:43:29 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        String str = my_string.length() >= is_prefix.length()? my_string.substring(0, is_prefix.length()): "";
+        if(str.equals(is_prefix)) return 1;
+        else return 0;
+    }
+}
