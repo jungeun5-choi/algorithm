05a58028fb96486ff7c35622d01a02dc3aa33126
Jungeun Choi <jungeun5.choi@gmail.com>
2023-08-15 19:56:46 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = Integer.toString(n);
+        for (int i = 0; i < str.length(); i++) {
+            answer += Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+        return answer;
+    }
+}
