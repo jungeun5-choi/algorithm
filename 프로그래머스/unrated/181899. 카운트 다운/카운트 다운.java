a2db1db2ab8fdb1ab6d74267d920be5c91219cbe
Jungeun Choi <jungeun5.choi@gmail.com>
2023-08-15 16:39:18 +0900

    [unrated] Title: 카운트 다운, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[start - end + 1];
+        int cnt = 0;
+        for (int i = start; i >= end; i--) {
+            answer[cnt++] = i;
+        }
+        return answer;
+    }
+}
