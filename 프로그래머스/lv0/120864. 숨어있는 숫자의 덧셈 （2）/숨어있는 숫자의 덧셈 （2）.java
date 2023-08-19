648bdb18ccb8c5bb51be8c6b270e581bcd91c26f
Jungeun Choi <jungeun5.choi@gmail.com>
2023-08-20 03:42:00 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.26 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-		String[] str = my_string.split("[a-zA-z]+");		
+		String[] str = my_string.replaceAll("[a-zA-Z]", " ").split(" ");
 		for (int i = 0; i < str.length; i++) {
-			if(str[i].matches("[0-9]+")){
+			if(!str[i].equals("")) {
 				answer += Integer.parseInt(str[i]);
-			}			
+			}
 		}
 		return answer;
     }
