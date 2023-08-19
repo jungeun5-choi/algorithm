5de7fb5ba38112ff5c14a016e53238367b30b0b9
Jungeun Choi <jungeun5.choi@gmail.com>
2023-08-20 02:40:42 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.42 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,11 +1,10 @@
+import java.util.*;
+
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-		answer += my_string.charAt(0);
-		for (int i = 1; i < my_string.length(); i++) {
-			if(!answer.contains(String.valueOf(my_string.charAt(i))))
-				answer += my_string.charAt(i);
-		}
-		return answer;
+        String[] answer = my_string.split("");
+        Set<String> set = new LinkedHashSet<String>(Arrays.asList(answer));
+
+        return String.join("", set);
     }
 }
