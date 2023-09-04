83cd09d9596cc54ecdcb565ce5df57b26c1b6d58
Jungeun Choi <jungeun5.choi@gmail.com>
2023-09-04 23:46:35 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+		int[] compare = arr.clone();
+		while (true) {
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] >= 50 && arr[i] % 2 == 0) arr[i] /= 2;					
+				else if (arr[i] < 50 && arr[i] % 2 != 0) arr[i] = arr[i] * 2 + 1;	
+			}
+			if (Arrays.equals(compare, arr)) {
+				break;
+			} else {
+				compare = arr.clone();
+				answer++;
+			}			
+		}
+		return answer;
+    }
+}
