0ae40a3bcdfc9325d6d99673cd1e5a0bb017b28d
Jungeun Choi <jungeun5.choi@gmail.com>
2023-09-23 11:44:30 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.05 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -2,20 +2,8 @@ class Solution {
     public int solution(int left, int right) {
 		int answer = 0;
 		for (int i = left; i <= right; i++) {
-			answer += divisorCounter(i) % 2 == 0 ? i : -i;
+			answer += i % Math.sqrt(i) == 0 ? -i : +i;
 		}
 		return answer;
-	}
-    
-    private int divisorCounter(int n) {
-		int count = 0;
-		for (int i = 1; i * i <= n; i++) {
-			if (i * i == n) {
-				count++;
-			} else if (n % i == 0) {
-				count += 2;
-			}
-		}
-		return count;
 	}
 }
