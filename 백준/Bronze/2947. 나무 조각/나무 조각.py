cd11e6d1d903279e26e32d4a1a91a9ff22211ce5
Jungeun Choi <jungeun5.choi@gmail.com>
2024-02-16 19:46:11 +0900

    [Bronze I] Title: 나무 조각, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+nums = list(map(int, input().split()))
+
+for i in range(len(nums)-1, 0, -1):
+    for j in range(i):
+        if nums == [1, 2, 3, 4, 5]:
+            break
+        if nums[j] > nums[j+1]:
+            nums[j], nums[j+1] = nums[j+1], nums[j]
+            print(*nums)
